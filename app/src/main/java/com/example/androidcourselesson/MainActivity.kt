34a0e9eee5d91fd87936c084ab29b1377e816b8d
Misha Blinov <spasibo1989@gmail.com>
2021-12-07 15:40:52 +0700

    Вывод чётной буквы алфавита

--- app/src/main/java/com/example/androidcourselesson/MainActivity.kt
+++ app/src/main/java/com/example/androidcourselesson/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("Вывод каждой чётной буквы английского алфавита:")
+
+        val abc = 'a'.rangeTo('z').toList()
+
+        for (char in 1 until abc.size step 2) {
+            println(abc[char])
+        }
     }
 }
