7afc5f39d7f7475ea99c2f4bccc06de9e72ee5ea
Misha Blinov <spasibo1989@gmail.com>
2021-12-07 20:17:08 +0700

    Стихотворение в жизненном цикле

--- app/src/main/java/com/example/androidcourselesson/MainActivity.kt
+++ app/src/main/java/com/example/androidcourselesson/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.androidcourselesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Ты видел деву на скале")
+        Log.d(TAG, "В одежде белой над волнами")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Когда, бушуя в бурной мгле,")
+        Log.d(TAG, "Играло море с берегами,")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Когда луч молний озарял")
+        Log.d(TAG, "Ее всечасно блеском алым")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "И ветер бился и летал")
+        Log.d(TAG, "С ее летучим покрывалом?")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Прекрасно море в бурной мгле")
+        Log.d(TAG, "И небо в блесках без лазури;")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Но верь мне: дева на скале")
+        Log.d(TAG, "Прекрасней волн, небес и бури.")
     }
 }
